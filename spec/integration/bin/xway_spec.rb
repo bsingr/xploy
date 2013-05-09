7b6c478c83676b52a32a3b13f64f369852a3243a
Jens Bissinger <mail@jens-bissinger.de>
2013-05-09 15:55:21 +0200

    Move bin spec to integration and fix it

--- spec/integration/bin/xway_spec.rb
+++ spec/integration/bin/xway_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require 'open3'
+require 'xway'
 
-describe 'xway' do
+describe Xway do
   it 'returns version string' do
-    exe = File.expand_path('../../bin/xway', File.dirname(__FILE__))
+    exe = File.expand_path('../../../bin/xway', File.dirname(__FILE__))
+    ENV['RUBYLIB'] = File.join(File.dirname(__FILE__), '../../../lib')
     stdin, stdout, stderr = Open3.popen3("#{exe}")
     stdout.readlines.to_s.should include(Xway::VERSION)
   end
