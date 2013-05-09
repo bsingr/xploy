d4bc7609257199da0e5587abcda66e843b7c87c2
Jens Bissinger <mail@jens-bissinger.de>
2013-05-09 16:11:59 +0200

    Refactor binary integration spec

--- spec/integration/bin/xway_spec.rb
+++ spec/integration/bin/xway_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 require 'open3'
-require 'xway'
 
-describe Xway do
-  it 'returns version string' do
+class XwayRunner
+  attr_reader :stdin, :stdout, :stderr
+  def run
     exe = File.expand_path('../../../bin/xway', File.dirname(__FILE__))
     ENV['RUBYLIB'] = File.join(File.dirname(__FILE__), '../../../lib')
-    stdin, stdout, stderr = Open3.popen3("#{exe}")
-    stdout.readlines.to_s.should include(Xway::VERSION)
+    @stdin, @stdout, @stderr = Open3.popen3("#{exe}")
   end
+end
+
+describe XwayRunner do
+  before { subject.run }
+
+  its('stdout.readlines.to_s') { should include(Xway::VERSION) }
 end
