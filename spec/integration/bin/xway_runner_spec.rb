94fc1243c1ca82e0965d92edefe5fead7a226dbb
Jens Bissinger <mail@jens-bissinger.de>
2013-05-10 08:27:38 +0200

    Move binary runner back to specs

--- spec/integration/bin/xway_runner_spec.rb
+++ spec/integration/bin/xway_runner_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 require 'open3'
-require 'xway/runner'
+
+module Xway
+  class Runner
+    attr_reader :stdin, :stdout, :stderr
+    def run
+      exe = File.expand_path('../../../bin/xway', File.dirname(__FILE__))
+      ENV['RUBYLIB'] = File.join(File.dirname(__FILE__), '../../../lib')
+      @stdin, @stdout, @stderr = Open3.popen3("#{exe}")
+    end
+  end
+end
 
 describe Xway::Runner do
   before { subject.run }
