cbc162049a3318d7d2f5ce1b1e7dbb38859b4972
Jens Bissinger <mail@jens-bissinger.de>
2013-05-12 19:34:51 +0200

    Cleanup api

--- lib/xway/api.rb
+++ lib/xway/api.rb
@@ -7,8 +7,9 @@ module Xway
     def request method_name, *args, &block
       parameter = Xway.parameter
       request = Endpoints.new.send(method_name, parameter[:app] || {})
+      http_options = request.http_options.merge(:debug => parameter[:debug])
       parameter[:servers].map do |server|
-        Http.new.request server, request.http_options.merge(:debug => parameter[:debug])
+        Http.new.request server, http_options
       end
     end
   end
