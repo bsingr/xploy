87d2100f9bba25e3efa48a90da4008b0e2f49d97
Jens Bissinger <mail@jens-bissinger.de>
2013-05-12 12:42:19 +0200

    Stub out http, endpoints and request for api specs

--- spec/lib/xway/api_spec.rb
+++ spec/lib/xway/api_spec.rb
@@ -3,79 +3,37 @@ require 'xway/api'
 
 describe Xway::Api do
   subject('api') { described_class.new }
-  context 'mock http' do
-    let('http') do
+  context 'mock http + request' do
+    let!('http') do
       double('Http').tap do |mock|
         mock.stub('request') do |server, request|
           Struct.new(:server, :request).new(server, request)
         end
+        Xway::Api::Http.stub('new').and_return(mock)
       end
     end
-    before { Xway::Api::Http.stub('new').and_return(http) }
+    let!('endpoints') do
+      double('Endpoints').tap do |mock|
+        mock.stub('request_foo') do |body|
+          ['get', '/foo', body: body]
+        end
+        Xway::Api::Endpoints.stub('new').and_return(mock)
+      end
+    end
     before do
       settings = double('Xway::Settings')
-      settings.stub('[]').with(:servers).and_return(['http://foo'])
+      settings.stub('[]').with(:servers).and_return(['http://foo',
+                                                     'http://bar'])
       Xway.stub('settings').and_return(settings)
     end
 
-    describe 'list' do
-      subject              { api.list }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
+    describe 'builds request for each server' do
+      subject { api.request_foo 'my body' }
 
-    describe 'create' do
-      subject              { api.create body: 'bar' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'find' do
-      subject              { api.find 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'update' do
-      subject              { api.update 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'delete' do
-      subject              { api.delete 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'log' do
-      subject              { api.log 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'start' do
-      subject              { api.start 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'stop' do
-      subject              { api.stop 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'restart' do
-      subject              { api.restart 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
-    end
-
-    describe 'redeploy' do
-      subject              { api.redeploy 'foo' }
-      its('first.server')  { should eq 'http://foo' }
-      its('first.request') { should be_kind_of(Xway::Api::Request) }
+      its('first.server')  { should eq('http://foo') }
+      its('first.request') { should eq(['get', '/foo', body: 'my body']) }
+      its('last.server')   { should eq('http://bar') }
+      its('last.request')  { should eq(['get', '/foo', body: 'my body']) }
     end
   end
 end
