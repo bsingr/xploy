4e6d6f89037342102c80609174678b1d8eca3760
Jens Bissinger <mail@jens-bissinger.de>
2013-05-12 19:50:46 +0200

    Cleanup api

--- lib/xway/api.rb
+++ lib/xway/api.rb
@@ -5,11 +5,21 @@ require 'xway/api/http'
 module Xway
   class Api
     def request method_name, *args, &block
-      parameter = Xway.parameter
-      request = Endpoints.new.send(method_name, parameter[:app] || {})
+      http = Http.new
+      request = build_request method_name
       parameter[:servers].map do |server|
-        Http.new.request server, request, parameter[:debug]
+        http.request server, request, parameter[:debug]
       end
+    end
+
+  private
+
+    def parameter
+      Xway.parameter
+    end
+
+    def build_request method_name
+      Endpoints.new.send(method_name, parameter[:app] || {})
     end
   end
 end
