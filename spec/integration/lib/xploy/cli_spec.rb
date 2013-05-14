ecdf46b64dc22a1145a9a0d4d065e4aa5d118cf5
Jens Bissinger <mail@jens-bissinger.de>
2013-05-14 19:38:33 +0200

    Add specs for cli

--- spec/integration/lib/xploy/cli_spec.rb
+++ spec/integration/lib/xploy/cli_spec.rb
@@ -6,4 +6,24 @@ describe Xploy::Cli do
     subject.instance_variable_get('@api').should be_instance_of(Xploy::Api)
     subject.instance_variable_get('@out').should eq STDOUT
   end
+
+  context 'mock api + out' do
+    let('api') do
+      double('api').tap do |mock|
+        mock.stub('request')
+      end
+    end
+    let('out') do
+      double('out').tap do |mock|
+        mock.stub('puts')
+      end
+    end
+    subject { described_class.new(api, out) }
+
+    it 'rescues MissingParameter' do
+      api.stub('request') { raise Xploy::MissingParameter, 'foo' }
+      out.should_receive('puts').with('foo')
+      subject.start
+    end
+  end
 end
