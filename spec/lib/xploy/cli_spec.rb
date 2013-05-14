30bac0ead6f1ebb86f530b8d9bf6f02863f1de2a
Jens Bissinger <mail@jens-bissinger.de>
2013-05-14 19:55:44 +0200

    Cleanup specs

--- spec/lib/xploy/cli_spec.rb
+++ spec/lib/xploy/cli_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'xploy/cli'
+require 'xploy/error'
 require 'xploy/version'
 
 describe Xploy::Cli do
@@ -9,10 +10,27 @@ describe Xploy::Cli do
       mock.stub('[]').with(:version).and_return(nil)
     end
   end
-  let('api') { double('api') }
+  let('api') { double('Xploy::Api') }
   let('out') { double('stdout').tap { |o| o.stub('puts') } }
   before { Xploy.stub('parameter').and_return(parameter) }
   subject { described_class.new api, out }
+
+  context 'default' do
+    let!('api_class') do
+      Xploy::Api = double('api_class').tap do |mock|
+        mock.stub('new').and_return(api)
+      end
+    end
+    subject { described_class.new }
+  
+    it 'uses Api' do
+      subject.instance_variable_get('@api').should eq api
+    end
+
+    it 'uses STDOUT' do
+      subject.instance_variable_get('@out').should eq STDOUT
+    end
+  end
   
   it 'prints usage per default' do
     parameter.should_receive('print_help!')
@@ -29,6 +47,12 @@ describe Xploy::Cli do
     api.should_receive('request').with('list').and_return('list result')
     out.should_receive('puts').with('list result')
     parameter.stub('rest').and_return(['list'])
+    subject.start
+  end
+
+  it 'rescues MissingParameter' do
+    parameter.stub('rest') { raise Xploy::MissingParameter, 'foo' }
+    out.should_receive('puts').with('foo')
     subject.start
   end
 end
